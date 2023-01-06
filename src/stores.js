e85a5d47fe75d471f0f2637d14c33b15da2df0bd
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-01-05 22:17:56 -0800

    Updated the gratitude of the customers

--- src/stores.js
+++ src/stores.js
@@ -4,16 +4,16 @@ export const FeedbackStore = writable([
   {
     id: 1,
     rating: 10,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+    text: "The service as amazing! The waiters were very prompt and the food was very good!"
   },
   {
     id: 2,
     rating: 9,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+    text: "I'm going to need to bring my family back here for a celebration!"
   },
   {
     id: 3,
     rating: 8,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+    text: "I loved the decore of the place. Very great European inspired design."
   }
 ]);
