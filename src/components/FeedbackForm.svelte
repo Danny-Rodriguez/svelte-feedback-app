62546b9bcb508c43375f09bb447e41cc3ff1e090
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-01-05 22:11:49 -0800

    store

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,11 +1,10 @@
 <script>
   import { v4 as uuidv4 } from "uuid";
-  import { createEventDispatcher } from "svelte";
+  import { FeedbackStore } from "../stores";
+
   import Card from "./Card.svelte";
   import Button from "./Button.svelte";
   import RatingSelect from "./RatingSelect.svelte";
-
-  const dispatch = createEventDispatcher();
 
   let text = "";
   let rating = 10;
@@ -32,7 +31,10 @@
         text,
         rating: +rating
       };
-      dispatch("add-feedback", newFeedback);
+
+      FeedbackStore.update(currentFeedback => {
+        return [newFeedback, ...currentFeedback];
+      });
 
       text = "";
     }
