b82ee1d1f7d2e2bc4d329a0f5c7b41a137e609da
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-01-05 21:10:07 -0800

    added buttons

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Card from "./Card.svelte";
+  import Button from "./Button.svelte";
+
+  let text = "";
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+
+  <form action="">
+    <!--Rating Select-->
+    <div class="input-group">
+      <input type="text" placeholder="Tell us something that keeps you coming back" />
+      <button>Send</button>
+    </div>
+  </form>
+</Card>
--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,8 @@
 <script>
-import FeedbackItem from "./FeedbackItem.svelte";
-export let feedback = []
-
-console.log(feedback)
+  import FeedbackItem from "./FeedbackItem.svelte";
+  export let feedback = [];
 </script>
 
 {#each feedback as fb (fb.id)}
-<FeedbackItem item={fb} />
+  <FeedbackItem item={fb} on:delete-feedback />
 {/each}
-
